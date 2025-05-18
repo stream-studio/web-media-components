a2d2192e6b6ca373dd358dd614bba7a631dbbe98
Ludovic Bouguerra <bouguerra.ludovic@gmail.com>
2025-05-18 14:53:23 +0200

    add subpath

--- demo/next.config.ts
+++ demo/next.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 const nextConfig: NextConfig = {
   output: "export",
   outputFileTracingRoot: path.join(__dirname, "../"),
+  basePath: "/web-media-components",
 };
 
 export default nextConfig;
