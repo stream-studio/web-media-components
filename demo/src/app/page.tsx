4cef78e8f62eb9e32817716b946fdfd0f44a338c
Ludovic Bouguerra <bouguerra.ludovic@gmail.com>
2025-05-18 14:58:30 +0200

    fix images links

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          src="/logo.png"
+          src="logo.png"
           alt="Stream Studio Logo logo"
           width={100}
           height={100}
@@ -32,13 +32,13 @@ export default function Home() {
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://docs.stream.studio"
+          href="https://stream.studio"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -53,7 +53,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
